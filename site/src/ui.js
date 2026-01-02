6b55c94b5ffb9d76c88fee627b145e43d08ac42d
edempo <elhadji.sy100@gmail.com>
2026-01-02 00:09:02 -0500

    intermediary

--- site/src/ui.js
+++ site/src/ui.js
@@ -1,4 +1,4 @@
-import { dfs, bfs } from "./graph.js";
+import { dfs } from "./graph.js";
 
 async function loadGraph() {
   const response = await fetch("/data/courses.json");
@@ -9,65 +9,47 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("course-input");
   const button = document.getElementById("search-button");
   const collapse_button = document.getElementById("collapsible-button");
+  const ul_prereqs = document.getElementById("prereqs-list");
+  const ul_coreqs = document.getElementById("coreqs-list");
+  const ul_reverse = document.getElementById("reverse-list");
+  const prereq_count = document.getElementById("prereq-count");
+  const coreq_count = document.getElementById("coreq-count");
+  const reverse_count = document.getElementById("reverse-count");
+  let prereqs = [];
+  let coreqs = [];
+  let reverses = [];
 
-  function render(ul, list, limit) {
-    let count = 0;
-    if (limit == -1) {
-      for (const c of list) {
-        const li = document.createElement("li");
-        if (
-          c != "None" &&
-          c != "No prerequisites for this course." &&
-          c != "No corequisites for this course."
-        ) {
-          const link = document.createElement("a");
-          link.href = "#";
-          link.textContent = c;
-          link.addEventListener("click", (e) => {
-            e.preventDefault();
-            input.value = c;
-            button.click();
-          });
-          li.appendChild(link);
-        } else {
-          li.textContent = c;
-        }
-        ul.appendChild(li);
+  function render(ul, list) {
+    for (const c of list) {
+      const li = document.createElement("li");
+      if (
+        c != "None" &&
+        c != "No prerequisites for this course." &&
+        c != "No corequisites for this course."
+      ) {
+        const link = document.createElement("a");
+        link.href = "#";
+        link.textContent = c;
+        link.addEventListener("click", (e) => {
+          e.preventDefault();
+          input.value = c;
+          button.click();
+        });
+        li.appendChild(link);
+      } else {
+        li.textContent = c;
       }
-    } else {
-      while (count != limit && count != list.length) {
-        const li = document.createElement("li");
-        //Making sure there's no link attached if there are no courses in the list
-        if (
-          list[count] == "No prerequisites for this course." ||
-          list[count] == "No corequisites for this course." ||
-          list[count] == "None"
-        ) {
-          li.appendChild(list[count]);
-          ul.appendChild(li);
-        } else {
-          const link = document.createElement("a");
-          link.href = "#";
-          link.textContent = list[count];
-          link.addEventListener("click", (e) => {
-            //Click logic for if the user clicks a link attached to any of the courses
-            e.preventDefault();
-            input.value = list[count];
-            button.click();
-          });
-          li.appendChild(link);
-          ul.appendChild(li);
-        }
-        count++;
-      }
+      ul.appendChild(li);
     }
   }
 
   function validateID(courseID, courseGraph) {
+    const error = document.getElementById("error");
     if (!courseGraph[courseID]) {
-      resDiv.innerText = "ERROR: Invalid Course ID.";
+      error.textContent = "ERROR: Invalid Course ID.";
       return false;
     }
+    error.textContent = "";
     return true;
   }
 
@@ -79,66 +61,75 @@ document.addEventListener("DOMContentLoaded", () => {
     return dfs(courseID, courseGraph, edgeKey);
   }
 
-  let limit = 8;
+  let state = "hidden";
+
   collapse_button.textContent = "Show";
   collapse_button.addEventListener("click", () => {
     reverse_count.textContent = "";
     ul_reverse.textContent = "";
-    if (limit == 8) {
-      limit = -1;
+    if (state === "hidden") {
+      state = "expanded";
       collapse_button.textContent = "Hide";
-      reverse_count.textContent = `Showing ${reverse_prereqs.length} out of ${reverse_prereqs.length} reverse perequisites.`;
+      reverse_count.textContent = `Showing ${reverses.length} out of ${reverses.length} reverse perequisites.`;
     } else {
-      limit = 8;
+      state = "hidden";
       collapse_button.textContent = "Show";
-      reverse_count.textContent = `Showing 8 out of ${reverse_prereqs.length} reverse perequisites.`;
+      reverse_count.textContent = `Showing 8 out of ${reverses.length} reverse perequisites.`;
     }
-    render(ul_reverse, reverse_prereqs, limit, state);
+
+    const visible = state === "expanded" ? reverses : reverses.slice(0, 8);
+    render(ul_reverse, visible);
   });
+
   loadGraph().then((courseGraph) => {
     button.addEventListener("click", () => {
-      const courseID = input.value;
+      const courseID = input.value.toUpperCase();
       if (validateID(courseID, courseGraph)) {
-        const prereqs = getEdgesBFS(courseID, courseGraph, "prereqs");
+        state = "hidden";
+        collapse_button.textContent = "Show";
+        prereqs = getEdgesBFS(courseID, courseGraph, "prereqs");
         if (prereqs.length == 0) {
           prereqs.push("No prerequisites for this course.");
         }
-        const coreqs = getEdgesDFS(courseID, courseGraph, "coreqs");
-        console.log(coreqs);
+
+        coreqs = getEdgesDFS(courseID, courseGraph, "coreqs");
         if (coreqs.length == 0) {
           coreqs.push("No corequisites for this course.");
         }
-        const reverse_prereqs = getEdgesDFS(
-          courseID,
-          courseGraph,
-          "reverse_prereqs",
-        );
-        console.log(reverse_prereqs);
-        if (reverse_prereqs.length == 0) {
-          reverse_prereqs.push("None");
+        reverses = getEdgesDFS(courseID, courseGraph, "reverse_prereqs");
+        if (reverses.length == 0) {
+          reverses.push("None");
         }
 
-        const ul_prereqs = document.getElementById("prereqs-list");
-        const ul_coreqs = document.getElementById("coreqs-list");
-        const ul_reverse = document.getElementById("reverse-list");
-        const prereq_count = document.getElementById("prereq-count");
-        const coreq_count = document.getElementById("coreq-count");
-        const reverse_count = document.getElementById("reverse-count");
         ul_prereqs.textContent = "";
         ul_coreqs.textContent = "";
         ul_reverse.textContent = "";
-        render(ul_prereqs, prereqs, -1);
-        render(ul_coreqs, coreqs, -1);
-        render(ul_reverse, reverse_prereqs, limit);
-        prereq_count.textContent = `${prereqs.length} prerequisites found.`;
-        coreq_count.textContent = `${coreqs.length} corequisites found.`;
-        if (reverse_prereqs.length < 8) {
-          reverse_count.textContent = `${reverse_prereqs.length} reverse prerequisites found.`;
+        if (prereqs[0] !== "No prerequisites for this course.") {
+          render(ul_prereqs, prereqs);
+          prereq_count.textContent = `${prereqs.length} prerequisites found.`;
         } else {
-          reverse_count.textContent =
-            limit == 8
-              ? `Showing 8 out of ${reverse_prereqs.length} reverse perequisites.`
-              : `Showing ${reverse_prereqs.length} out of ${reverse_prereqs.length} reverse perequisites.`;
+          prereq_count.textContent = `No prerequisites found.`;
+        }
+        if (coreqs[0] !== "No corequisites for this course.") {
+          render(ul_coreqs, coreqs);
+          coreq_count.textContent = `${coreqs.length} corequisites found.`;
+        } else {
+          coreq_count.textContent = `No corequisites found.`;
+        }
+        if (reverses[0] !== "None") {
+          const visible =
+            state === "expanded" ? reverses : reverses.slice(0, 8);
+          if (reverses.length < 8) {
+            reverse_count.textContent = `${reverses.length} reverse prerequisites found.`;
+          } else {
+            reverse_count.textContent =
+              state === "hidden"
+                ? `Showing 8 out of ${reverses.length} subsequent courses.`
+                : `Showing ${reverses.length} out of ${reverses.length} subsequent courses.`;
+          }
+          render(ul_reverse, visible);
+        } else {
+          reverse_count.textContent = `No subsequent courses found.`;
         }
       }
     });
