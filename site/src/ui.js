4ae9d025de4fc16cee16ad175f9401f43c4c616b
edempo <elhadji.sy100@gmail.com>
2026-01-01 20:27:09 -0500

    intermediary

--- site/src/ui.js
+++ site/src/ui.js
@@ -5,79 +5,62 @@ async function loadGraph() {
   const graph = await response.json();
   return graph;
 }
-
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("course-input");
   const button = document.getElementById("search-button");
+  const collapse_button = document.getElementById("collapsible-button");
 
-  function displayResults({ prereqs, coreqs, reverse_prereqs }) {
-    const ul_prereqs = document.getElementById("prereqs-list");
-    const ul_coreqs  =  document.getElementById("coreqs-list");
-    const ul_reverse =  document.getElementById("reverse-list");
-    ul_prereqs.textContent = "";
-    ul_coreqs.textContent = "";
-    ul_reverse.textContent = "";
-
-    for (const c of prereqs) {
-      const li = document.createElement("li");
-      if (c != "No prerequisites for this course.") {
-        const link = document.createElement("a");
-        link.href = "#";
-        link.textContent = c;
-        link.addEventListener("click", (e) => {
-          e.preventDefault();
-          input.value = c;
-          button.click();
-        });
-        li.appendChild(link);
-      } else {
-        li.textContent = c;
+  function render(ul, list, limit) {
+    let count = 0;
+    if (limit == -1) {
+      for (const c of list) {
+        const li = document.createElement("li");
+        if (
+          c != "None" &&
+          c != "No prerequisites for this course." &&
+          c != "No corequisites for this course."
+        ) {
+          const link = document.createElement("a");
+          link.href = "#";
+          link.textContent = c;
+          link.addEventListener("click", (e) => {
+            e.preventDefault();
+            input.value = c;
+            button.click();
+          });
+          li.appendChild(link);
+        } else {
+          li.textContent = c;
+        }
+        ul.appendChild(li);
       }
-
-      ul_prereqs.appendChild(li);
-    }
-
-
-    for (const c of coreqs) {
-      const li = document.createElement("li");
-      if (c != "No corequisites for this course.") {
-        const link = document.createElement("a");
-        link.href = "#";
-        link.textContent = c;
-        link.addEventListener("click", (e) => {
-          e.preventDefault();
-          input.value = c;
-          button.click();
-        });
-        li.appendChild(link);
-      } else {
-        li.textContent = c;
+    } else {
+      while (count != limit && count != list.length) {
+        const li = document.createElement("li");
+        //Making sure there's no link attached if there are no courses in the list
+        if (
+          list[count] == "No prerequisites for this course." ||
+          list[count] == "No corequisites for this course." ||
+          list[count] == "None"
+        ) {
+          li.appendChild(list[count]);
+          ul.appendChild(li);
+        } else {
+          const link = document.createElement("a");
+          link.href = "#";
+          link.textContent = list[count];
+          link.addEventListener("click", (e) => {
+            //Click logic for if the user clicks a link attached to any of the courses
+            e.preventDefault();
+            input.value = list[count];
+            button.click();
+          });
+          li.appendChild(link);
+          ul.appendChild(li);
+        }
+        count++;
       }
-
-      ul_coreqs.appendChild(li);
     }
-
-
-    for (const c of reverse_prereqs) {
-      const li = document.createElement("li");
-      if (c != "None") {
-        const link = document.createElement("a");
-        link.href = "#";
-        link.textContent = c;
-        link.addEventListener("click", (e) => {
-          e.preventDefault();
-          input.value = c;
-          button.click();
-        });
-        li.appendChild(link);
-      } else {
-        li.textContent = c;
-      }
-
-      ul_reverse.appendChild(li);
-    }
-
-
   }
 
   function validateID(courseID, courseGraph) {
@@ -96,6 +79,22 @@ document.addEventListener("DOMContentLoaded", () => {
     return dfs(courseID, courseGraph, edgeKey);
   }
 
+  let limit = 8;
+  collapse_button.textContent = "Show";
+  collapse_button.addEventListener("click", () => {
+    reverse_count.textContent = "";
+    ul_reverse.textContent = "";
+    if (limit == 8) {
+      limit = -1;
+      collapse_button.textContent = "Hide";
+      reverse_count.textContent = `Showing ${reverse_prereqs.length} out of ${reverse_prereqs.length} reverse perequisites.`;
+    } else {
+      limit = 8;
+      collapse_button.textContent = "Show";
+      reverse_count.textContent = `Showing 8 out of ${reverse_prereqs.length} reverse perequisites.`;
+    }
+    render(ul_reverse, reverse_prereqs, limit, state);
+  });
   loadGraph().then((courseGraph) => {
     button.addEventListener("click", () => {
       const courseID = input.value;
@@ -118,7 +117,29 @@ document.addEventListener("DOMContentLoaded", () => {
         if (reverse_prereqs.length == 0) {
           reverse_prereqs.push("None");
         }
-        displayResults({ prereqs, coreqs, reverse_prereqs });
+
+        const ul_prereqs = document.getElementById("prereqs-list");
+        const ul_coreqs = document.getElementById("coreqs-list");
+        const ul_reverse = document.getElementById("reverse-list");
+        const prereq_count = document.getElementById("prereq-count");
+        const coreq_count = document.getElementById("coreq-count");
+        const reverse_count = document.getElementById("reverse-count");
+        ul_prereqs.textContent = "";
+        ul_coreqs.textContent = "";
+        ul_reverse.textContent = "";
+        render(ul_prereqs, prereqs, -1);
+        render(ul_coreqs, coreqs, -1);
+        render(ul_reverse, reverse_prereqs, limit);
+        prereq_count.textContent = `${prereqs.length} prerequisites found.`;
+        coreq_count.textContent = `${coreqs.length} corequisites found.`;
+        if (reverse_prereqs.length < 8) {
+          reverse_count.textContent = `${reverse_prereqs.length} reverse prerequisites found.`;
+        } else {
+          reverse_count.textContent =
+            limit == 8
+              ? `Showing 8 out of ${reverse_prereqs.length} reverse perequisites.`
+              : `Showing ${reverse_prereqs.length} out of ${reverse_prereqs.length} reverse perequisites.`;
+        }
       }
     });
   });
