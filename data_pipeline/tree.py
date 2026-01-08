edf2f6cef3f12c1f4a8dc47f0259c271dcf87b94
edempo <elhadji.sy100@gmail.com>
2026-01-08 02:55:39 -0500

    Tokenizer finished

--- data_pipeline/tree.py
+++ data_pipeline/tree.py
@@ -25,16 +25,7 @@ def tokenize(s: str) -> list:
                 tokens.append(course.group())
     return tokens
 
-prereq1 = "prereqs: 1 course with a minimum grade of C- from (CMSC414, CMSC417, CMSC420, CMSC430, CMSC433, CMSC435, ENEE440, ENEE457); and permission of ENGR-Electrical & Computer Engineering department; and (ENEE350, CMSC330, and CMSC351)."
-prereq2 = "prereqs: 1 course with a minimum grade of C- from (CMSC412, CMSC417, CMSC420, CMSC430, CMSC433, ENEE447); and permission of CMNS-Computer Science department."
-prereq3 = "prereqs: Minimum grade of C- in ENEE322 or ENEE323."
-
-print(f"{tokenize(prereq1)}\n")
-print(f"{tokenize(prereq2)}\n")
-print(f"{tokenize(prereq3)}\n")
-
-def parse(tokens: list) -> dict:
-    pass
+#def parse(tokens: list) -> dict:
    # courseTree = {}
    # for token in tokens:
    #     switch case
