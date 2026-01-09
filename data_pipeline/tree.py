890bf3fe2c707ff3023efb0b9b87a09804d531ce
edempo <elhadji.sy100@gmail.com>
2026-01-09 17:36:34 -0500

    intermediary

--- data_pipeline/tree.py
+++ data_pipeline/tree.py
@@ -1,5 +1,10 @@
 import re
 
+class courseNode:
+    def __init__(self, type_, children):
+        self.type = type_
+        self.children = children
+
 def treeify(s):
     return parse(tokenize(s))
 
@@ -25,10 +30,23 @@ def tokenize(s: str) -> list:
                 tokens.append(course.group())
     return tokens
 
-#def parse(tokens: list) -> dict:
-   # courseTree = {}
-   # for token in tokens:
-   #     switch case
-   # 
+s = "1 course with a minimum grade of C- from (CMSC414, CMSC417, CMSC420, CMSC430, CMSC433, CMSC435, ENEE440, ENEE457); and permission of ENGR-Electrical & Computer Engineering department; and (ENEE350, CMSC330, and CMSC351).";
+print(tokenize(s))
+#for token in tokens:
+#if LPAREN, while not right paren follow parse rules for all other token types
+#if RPAREN, break out of the loop and just go back to the main parse loop
+#if COURSE, add course to courseTree children
+#if OR, call or parser; if no AND has been found yet, set type of node to OR
+#if AND, call and parser; if AND is outside of PAREN, then  make type of courseTree AND
 
+#I will have a paren flag letting me know if I'm in a set of parenthesis
+#I will make a subtree for all tre
+def parse(tokens: list) -> courseNode:
+    for i in range(len(tokens)):
+        if tokens[i] == "OR":
+            parse_or(tokens[i:])
+    return courseNode(None, [])
+
+
+def parse_or(token_slice):
 
